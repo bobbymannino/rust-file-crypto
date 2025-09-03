6cd023d7bc0144c96d7efcbd0b119047013686cc
Bob Mannino <manninobobby@icloud.com>
2025-09-03 13:10:12 +0100

    feat: add barebones password saving/getting using keyring

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use keyring::Entry;
+
+fn main() -> keyring::Result<()> {
+    let entry = Entry::new("my-service", "my-name")?;
+
+    entry.set_password("password-here")?;
+
+    let pwd = entry.get_password()?;
+
+    println!("Password is {pwd}");
+
+    entry.delete_credential()?;
+
+    Ok(())
 }
